16fab44edd6351a1046c0d46535ad29a506e9bda
hss707 <120069806+hss707@users.noreply.github.com>
2022-12-11 21:46:34 +0900

    매크로파일 업로드

---
+++ Nam/No1/1-5_3ch_2_115/macro.ino
@@ -0,0 +1,34 @@
+void test1() {
+  anum = random(0, 7);
+  jnum = random(0, 2);
+  jjnum = random(0, 3);
+
+  _p_p();
+  rd_50(600);
+  S1();
+  jump_attack_R();
+  jump_attack_1();
+  S14();
+  jump_attack_1();
+  jump_df_R();
+  jump_attack_L();
+  jump_attack_1();
+  Doublejump();
+  S2();
+  Downjumpattack();
+  _p_p();
+  rd_50(400);
+
+  _p_p();
+  jump_attack_R();
+  jump_attack_3();
+  S15();
+  jump_attack_L();
+  jump_attack_2();
+  S15();
+  jump_attack_2();
+  rd_30(500); 
+  Downjumptread();
+
+  Reset();
+}
